e8545d1047aa95f7ae45d22b4a80652022cea7a5
Linas Nikiperavičius <linas@linasdev.com>
2026-05-23 20:02:06 +0300

    Re-structure a bit

--- modrad-server/src/listener/udp/mod.rs
+++ modrad-server/src/listener/udp/mod.rs
@@ -1,9 +1,9 @@
 use crate::listener::udp::config::UdpRadiusListenerConfig;
 use crate::listener::{RadiusListener, RadiusListenerError};
 use async_trait::async_trait;
-use modrad_radius::packet::RadiusPacket;
 use std::net::SocketAddr;
 use tokio::net::UdpSocket;
+use modrad_radius::packet::radius::RadiusPacket;
 
 pub mod config;
 
