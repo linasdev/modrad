46f1b3c797255c799c6fcb210e874ff282674201
Linas Nikiperavičius <linas@linasdev.com>
2026-06-01 20:01:56 +0300

    Make all metadata Send

--- modrad-radius/src/packet/metadata.rs
+++ modrad-radius/src/packet/metadata.rs
@@ -1,6 +1,6 @@
 use std::any::Any;
 
-pub trait RadiusPacketMetadata {
+pub trait RadiusPacketMetadata: Send {
     fn as_any(&self) -> &dyn Any;
     fn into_any(self: Box<Self>) -> Box<dyn Any>;
 }
